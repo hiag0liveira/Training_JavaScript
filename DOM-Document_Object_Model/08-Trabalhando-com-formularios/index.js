635ca9ddfe14ee09bd48b726427601ec8b94522f
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-02-25 23:14:39 -0300

    Update index.js

--- DOM-Document_Object_Model/08-Trabalhando-com-formularios/index.js
+++ DOM-Document_Object_Model/08-Trabalhando-com-formularios/index.js
@@ -1,11 +1,34 @@
-function register(element) {
-  const username = element.children.username.value
-  const password = element.children.password.value
-  const passwordConfirmation = element.children.passwordConfirmation.value
+const form = document.getElementById("orderForm")
 
-  if (password === passwordConfirmation) {
-    alert("Usuário " + username + " registrado!")
-  } else {
-    alert("As senhas não conferem")
-  }
-}
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault()
+
+  const name = document.querySelector("input[name='name']").value
+  const address = document.querySelector("input[name='address']").value
+  const breadType = document.querySelector("select[name='breadType']").value
+  const main = document.querySelector("input[name='main']").value
+  const observations = document.querySelector("textarea[name='observations']").value
+
+  let salad = ""
+  document.querySelectorAll("input[name='salad']:checked").forEach(function (item) {
+    salad += " - " + item.value + "\n"
+  })
+
+  console.log({
+    name,
+    address,
+    breadType,
+    main,
+    salad,
+    observations
+  })
+
+  alert(
+    "Pedido Realizado!" +
+    "\nNome do cliente: " + name +
+    "\nEndereço de entrega: " + address +
+    "\nTipo de pão: " + breadType +
+    "\nRecheio: \n - " + main + "\n" + salad +
+    "Observações: " + observations
+  )
+})

0ef4201ac34c552b252e46c0b59f59110d52dcaa
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-02-28 13:12:02 -0300

    Update index.js

--- DOM-Document_Object_Model/11-Manipulando-atributos-com-js/index.js
+++ DOM-Document_Object_Model/11-Manipulando-atributos-com-js/index.js
@@ -1,21 +1,28 @@
-// Manipulando estilos diretamente pela propriedade style
-function useLightTheme() {
-  document.body.style.color = '#212529'
-  document.body.style.backgroundColor = '#f1f5f9'
-}
+const input = document.getElementById('input')
 
-function useDarkTheme() {
-  document.body.style.color = '#f1f5f9'
-  document.body.style.backgroundColor = '#212529'
-}
+document.getElementById('value').addEventListener('click', function () {
+  input.value = 'Olá, mundo!'
 
-// Manipulando estilos através das classes utilizadas pelo CSS
-function switchTheme() {
-  document.body.classList.toggle('is-light')
-  document.body.classList.toggle('is-dark')
-}
+  console.log(input.value)
+  console.log(input.getAttribute('value'))
+})
 
-// Adicionando os eventos
-document.getElementById('lightBtn').addEventListener('click', useLightTheme)
-document.getElementById('darkBtn').addEventListener('click', useDarkTheme)
-document.getElementById('switchBtn').addEventListener('click', switchTheme)
+document.getElementById('type').addEventListener('click', function () {
+  // input.type = input.type !== 'date' ? 'date' : 'text'
+  input.setAttribute('type', 'radio')
+})
+
+document.getElementById('placeholder').addEventListener('click', function () {
+  input.placeholder = 'Digita algo...'
+})
+
+document.getElementById('disable').addEventListener('click', function () {
+  input.setAttribute('disabled', !input.disabled)
+})
+
+document.getElementById('data').addEventListener('click', function () {
+  const data = input.dataset.somethingElse
+  console.log("O valor do atributo data-something-else é: " + data)
+  input.dataset.somethingElse = 'Algum outro valor'
+  console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse)
+})

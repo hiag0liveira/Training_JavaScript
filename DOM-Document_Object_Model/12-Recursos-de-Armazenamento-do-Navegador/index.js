cc819337a43ab948189bd3eb5cd734373e5c7b61
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-09 14:17:19 -0300

    Update index.js

--- DOM-Document_Object_Model/12-Recursos-de-Armazenamento-do-Navegador/index.js
+++ DOM-Document_Object_Model/12-Recursos-de-Armazenamento-do-Navegador/index.js
@@ -1,21 +1,55 @@
-// Manipulando estilos diretamente pela propriedade style
-function useLightTheme() {
-  document.body.style.color = '#212529'
-  document.body.style.backgroundColor = '#f1f5f9'
-}
+// Usando o sessionStorage
 
-function useDarkTheme() {
-  document.body.style.color = '#f1f5f9'
-  document.body.style.backgroundColor = '#212529'
-}
+document.getElementById('sessionBtn').addEventListener('click', function () {
+  const input = document.getElementById('session')
+  sessionStorage.setItem('info', input.value)
+  input.value = ''
+})
 
-// Manipulando estilos através das classes utilizadas pelo CSS
-function switchTheme() {
-  document.body.classList.toggle('is-light')
-  document.body.classList.toggle('is-dark')
-}
+document.getElementById('readSesssion').addEventListener('click', function () {
+  const info = sessionStorage.getItem('info')
+  alert('A informação salva é: ' + info)
+})
 
-// Adicionando os eventos
-document.getElementById('lightBtn').addEventListener('click', useLightTheme)
-document.getElementById('darkBtn').addEventListener('click', useDarkTheme)
-document.getElementById('switchBtn').addEventListener('click', switchTheme)
+
+
+
+// Usando o localStorage
+
+document.getElementById('localBtn').addEventListener('click', function () {
+  const input = document.getElementById('local')
+  localStorage.setItem('text', input.value)
+  input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function () {
+  const t = localStorage.getItem('text')
+  alert('O texto salvo no local storage é: ' + t)
+})
+
+
+
+
+// Usando cookies
+
+document.getElementById('cookieBtn').addEventListener('click', function () {
+  const input = document.getElementById('cookie')
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'info=' + input.value + ';'
+  const expiration = 'expires=' + new Date(2022,9, 9) + ';'
+  const path = 'path=/;'
+  document.cookie = cookie + expiration + path
+  input.value = ''
+  console.log(document.cookie)
+})
+
+document.getElementById('cookie2Btn').addEventListener('click', function () {
+  const input = document.getElementById('cookie2')
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'text=' + input.value + ';'
+  const expiration = 'expires=' + new Date(2022, 8, 9) + ';'
+  const path = 'path=/;'
+  document.cookie = cookie + expiration + path
+  input.value = ''
+  console.log(document.cookie)
+})

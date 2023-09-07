6ab7b971e18a085f60e420cd6433a439dea0ae61
Hiago Mendes <hiagophd@gmail.com>
2023-09-07 16:21:57 -0300

    Implementação de novo arquivos

---
+++ Modernizing/09-Modulos_CommonJS/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+/* const render = require("./render")
+const store = require("./store") */
+
+console.log("Aplicação iniciada.")
+render()
+store()
+console.log("Aplicação finalizada.")
+
+/* node index.js */

742fcb3c06754b5187e136ba33f56ba370289d12
Hiago Mendes <hiagophd@gmail.com>
2023-09-12 21:02:40 -0300

    Implementacao POO

---
+++ POO/01-Obejtos_JS/index1.js
@@ -0,0 +1,18 @@
+const book = {
+    title: "Eragon",
+    pages: 468,
+    published: true,
+    inStock: 20,
+    tags: ["fantasy", "adventure", "medieval"],
+    author: {
+        name: "Christopher Paolini"
+    },
+    addOnStock(quantity) {
+        this.inStock += quantity
+    },
+    save: function () {
+        // Save book data to database
+    }
+}
+
+console.log(book)

7f7505df11502406425081e4302558ab19b38dab
Hiago Mendes <hiagophd@gmail.com>
2023-09-24 13:45:34 -0300

    Implementacao Promisses

---
+++ 05-Topicos_Avancados/12-Rejeitando_Promise_Funcoes_Async/index.js
@@ -0,0 +1,28 @@
+//Mas as funções async também permitem rejeitar uma promise. 
+//Podemos fazer isso através do objeto global Promise e seu método .reject():
+
+async function asyncSum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('arguments must be of type number')
+    }
+    return a + b
+}
+
+async function asyncSubtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('arguments must be of type number')
+    }
+    return a - b
+}
+
+asyncSum(50, null).then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+})
+
+asyncSubtract(50, 33).then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+})
